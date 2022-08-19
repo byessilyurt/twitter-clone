b35ddce7c61a875ad9dbfd085228a0dae11907c6
yessilyurt <y.yesilyurt14@gmail.com>
2022-08-19 12:13:53 +0100

    git ignore working

--- server/index.js
+++ server/index.js
@@ -1,30 +1,29 @@
-import express from 'express'
+import express from "express";
 const app = express();
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import userRoute from "./routes/users.js" 
-import authRoute from "./routes/auth.js" 
-import postRoute from "./routes/posts.js" 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import postRoute from "./routes/posts.js";
 
 dotenv.config();
 mongoose.connect(
   "mongodb+srv://yessilyurt:<password>@twitter-cluster.nwrl0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  { useNewUrlParser: true,useUnifiedTopology: true },
+  { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log("mongodb connected");
+    ("mongodb connected");
   }
 );
-
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
 });
 
-app.get("/", (req,res) => {
-  res.send("Homepage")
-})
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
 
 //middleware
 app.use(express.json());
